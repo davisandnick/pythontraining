6c8b1a63d5a8cc7fff52ae70e4b914b1ba918b73
Nicholas Day <nick@nickendo.com>
2014-07-21 14:52:16 -0400

    update
    new stuff

--- pythonthings.py
+++ pythonthings.py
@@ -1,1 +1,9 @@
-print("hello")
+hello = input("What is your name?")
+hello = hello.lower()
+if hello == "davis" or hello == "nick":
+    print("your name is" + hello)
+else:
+    print("you're not my friend")
+
+for i in range(1,10):
+    print("hello")
